211c8c97b5c78360df61dad574ed46d010d34a0f
Viking0672 <yavkata06@yahoo.com>
2023-10-05 00:14:51 +0300

    Commit

---
+++ src/main/java/data/types/Test.java
@@ -0,0 +1,12 @@
+package data.types;
+
+public class Test {
+    public static void main(String[] args) {
+        Variables o1 = new Variables();
+        Variables o2 = new Variables();
+        o1.age = 20;
+        o2.age = 17;
+
+        o1.printAge();
+    }
+}
